65a26f6cf76a8e029c607b166b62fec02210c884
Sushanth <sushanth.gupta@ionage.in>
2026-03-14 15:06:57 +0530

    first commit

---
+++ f_string_example.py
@@ -0,0 +1,12 @@
+project_name = 'hello_world'
+file_name = 'hello.py'
+py_version = '3.14'
+prompt_to_create_apython_project = f""" 
+    Please create a python project
+    with name {project_name} and add
+    a file named {file_name} in it.
+    In the file {file_name}, write a code snippet
+    that demonstrates the use print, also use
+    python version {py_version}"""
+
+print(prompt_to_create_apython_project)
---
+++ zip_example.py
@@ -0,0 +1,5 @@
+list1 = ["name", "place"]
+list2 = ["alice","goa"]
+
+dict1 = dict(zip(list1,list2))
+print(dict1)
